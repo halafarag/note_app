fbb0a9502ef3dcd407c60dda55aee7a548f4ecb1
Hala Farag <113291188+halafarag@users.noreply.github.com>
2023-09-10 15:47:40 +0300

    add bloc state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/screens/notes_screen.dart';
 import 'package:notes_app/widgets/constants.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const MyApp());
 }
 
